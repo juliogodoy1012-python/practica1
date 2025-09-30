e38eb91d7cff04d2d60a418348ff5b1800206fd6
Julio Godoy <juliogodoy1012@gmail.com>
2025-09-30 11:55:24 -0600

    coment

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ console.log(saludar("Mundo"));
 
 module.exports = saludar;
 // fin de la funcion Julio
+
+function sumar(a, b) {
+  return a + b;
+}
+
+console.log("Resultado {sumar(5, 3)"); // El resultado es: 8
