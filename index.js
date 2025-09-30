45d213215058701562d2f73dc752d967553fa885
Julio Godoy <juliogodoy1012@gmail.com>
2025-09-30 11:49:48 -0600

    Actualización

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function sumar(a, b) {
 // Ejemplo de uso
 let resultado = sumar(5, 3);
 console.log("El resultado es:", resultado); // El resultado es: 8
+
+
+
+function restar(a, b) {
+  return a - b;
+}
+
+console.log("La resta es:", restar(10, 4)); // La resta es: 6
+
